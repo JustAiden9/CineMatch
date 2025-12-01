7cac9d5fd4340786581069d8fd4ad0b189118561
Aiden <aidenbak9@gmail.com>
2025-12-01 10:13:53 -0600

    Initial Commit

---
+++ CineMatch/CineMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineMatchApp.swift
+//  CineMatch
+//
+//  Created by Aiden Baker on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CineMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CineMatchTests/CineMatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  CineMatchTests.swift
+//  CineMatchTests
+//
+//  Created by Aiden Baker on 12/1/25.
+//
+
+import Testing
+@testable import CineMatch
+
+struct CineMatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
